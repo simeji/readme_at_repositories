9ba8a8669c86e06c08d931db5a342a81a0050b10
Raphael Kallensee <raphael@kallensee.name>
2016-05-27 18:56:44 +0200

    Fix non-working settings because of protected_attributes gem.

--- app/models/rar_project_setting.rb
+++ app/models/rar_project_setting.rb
@@ -1,3 +1,7 @@
 class RarProjectSetting < ActiveRecord::Base
   unloadable
+
+  if defined?(ProtectedAttributes) || ::ActiveRecord::VERSION::MAJOR < 4
+    attr_accessible :project_id, :position, :show
+  end
 end
